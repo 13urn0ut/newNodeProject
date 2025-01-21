73d5dccb530deca335c8f8d04146478cb53354ca
13urn0ut <nitrojunky@gmail.com>
2025-01-21 11:01:59 +0200

    Add not found route to app.js

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.use(express.json());
 
 app.use("/api/v1", Router);
 
+app.all("*", (req, res) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
